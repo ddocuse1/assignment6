3b0eef947e5c473c56d435c1e8bd57eb460d4662
Dustin Docusen <dustindocusen@public-68-131.emich.edu>
2025-10-30 19:23:36 -0400

    v4

--- central.py
+++ central.py
@@ -1,85 +1,45 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-k = 8.987e9
-q1 = -1.602e-19
-q2 = 1.602e-19
-x1 = -0.05
-x2 = -x1
-xlim = 0.1
-ylim = 0.1
-dx = dy = 0.001
-rows = cols = int(1+ 2*xlim/dx)
+s1 = 1
+s2 = 0.5
+s3 = 0.1
+n1 = int(4/s1)+1
+n2 = int(4/s2)+1
+n3 = int(4/s3)+1
+d1 = []
+d2 = []
+d3 = []
 
-#quiver
-xquiver = yquiver = 25
-rquiver = np.zeros((xquiver,yquiver))
-dquiv = 2*xlim/xquiver
-Xquiv, Yquiv  = np.meshgrid(np.arange(-xlim,xlim+dquiv,dquiv),np.arange(-ylim,ylim+dquiv,dquiv))
+x1 = np.linspace(-2,2, n1)
+x2 = np.linspace(-2,2, n2)
+x3 = np.linspace(-2,2, n3)
+xA = np.linspace(-2,2, 100)
 
-Rquiv1 = np.sqrt((Xquiv-x1)**2+Yquiv**2)
-Rquiv2 = np.sqrt((Xquiv-x2)**2+Yquiv**2)
-Rquiv1[Rquiv2 < 0.001] = 0.001
-Rquiv1[Rquiv2 < 0.001] =0.001
+def func(x):
+    return 2 + 0.75*np.tanh(2*x)
+for i in range(n1):
+    if i+1 == n1 or i+1 == 1:
+        continue
+    else:
+        d1.append((func(x1[i+1])-func(x1[i-1]))/(x1[i+1]-x1[i-1]))
+for i in range(n2):
+    if i+1 == n2 or i+1 == 1:
+        continue
+    else:
+        d2.append((func(x2[i+1])-func(x2[i-1]))/(x2[i+1]-x2[i-1]))
+for i in range(n3):
+    if i+1 == n3 or i+1 == 1:
+        continue
+    else:
+        d3.append((func(x3[i+1])-func(x3[i-1]))/(x3[i+1]-x3[i-1]))
+        
+def funcprime(x):
+    return (3/2)*(1/np.cosh(2*x))**2
 
-Exq1 = -k*q1*(Xquiv-x1)*Rquiv1**(-3)
-Eyq1 = -k*q1*Yquiv*Rquiv1**(-3)
-Exq2 = -k*q2*(Xquiv-x2)*Rquiv2**(-3)
-Eyq2 = -k*q2*Yquiv*Rquiv2**(-3)
-Exq = Exq1 + Exq2
-Eyq = Eyq1 + Eyq2
-Eq = np.sqrt(Exq**2+Eyq**2)
-
-Eqmax = np.percentile(Eq, 85)  
-maskq = Eq > Eqmax
-Exq[maskq] = np.nan
-Eyq[maskq] = np.nan
-
-#contour
-r_array = np.zeros((rows,cols))
-x = np.arange(-xlim,xlim+dx, dx)
-y = np.arange(-ylim,ylim+dx, dx)
-X, Y = np.meshgrid(x,y)
-
-R1 = np.sqrt((X-x1)**2+Y**2)
-R2 = np.sqrt((X-x2)**2+Y**2)
-R1[R1 < 0.001] = 0.001
-R2[R2 < 0.001] = 0.001
-
-V1 = k*q1/R1
-V2 = k*q2/R2
-V = V1 + V2
-
-Ex1 = k*q1*(X-x1)*R1**(-3)
-Ey1 = k*q1*Y*R1**(-3)
-Ex2 = k*q2*(X-x2)*R2**(-3)
-Ey2 = k*q2*Y*R2**(-3)
-
-Ex = Ex1 + Ex2
-Ey = Ey1 + Ey2
-E = np.sqrt(Ex**2 + Ey**2)
-
-Emax = np.percentile(E, 99)  
-mask = E > Emax
-Ex[mask] = np.nan
-Ey[mask] = np.nan
-print(V1,V2)
-
-#plotting
-fig, ax = plt.subplots(figsize=(6,5))  
-
-cf = ax.contourf(X, Y, V, levels=100, cmap='coolwarm')
-plt.colorbar(cf, ax=ax, label="Electric Potential (V)")
-ax.axis('equal')
-ax.set_xlabel("x (m)")
-ax.set_ylabel("y (m)")
-ax.set_title("Electric Potential")
-plt.savefig('potential.png')
-q = ax.quiver(Xquiv, Yquiv, Exq, Eyq, scale=1.8e-5, color='black')
-ax.quiverkey(q, .9, -.1,1.1e-6,r'$E = 10^{-6} V/m$',labelpos='W')
-ax.set_title("Electric Potential + Field")
-plt.savefig('efield.png')
-
-
-
+plt.plot(x1[1:-1],d1, label='1')
+plt.plot(x2[1:-1],d2, label='0.5')
+plt.plot(x3[1:-1],d3, label='0.1')
+plt.plot(xA, funcprime(xA), linestyle='--')
+plt.legend(title='Step Size')
 plt.show()
